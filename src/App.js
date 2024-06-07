743f02cc629456a06df84a069b197b594f93ea85
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-07 14:20:41 +0530

    added routers

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import AddBook from './components/AddBook';
 import SearchBook from './components/SearchBook';
 import DeleteBookk from './components/DeleteBookk';
 import ViewAll from './components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddBook/>
-      <SearchBook/>
-      <DeleteBookk/>
-      <ViewAll/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddBook/>}/>
+        <Route path='/searchbook' element={<SearchBook/>}/>
+        <Route path='/deletebook' element={<DeleteBookk/>}/>
+        <Route path='/viewall' element={<ViewAll/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
